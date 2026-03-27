aeaa4ca736eaada6b5583f27adc1b691e9207613
HexRaindrop <jrmikey2006@gmail.com>
2026-03-27 17:02:33 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ from dataclasses import dataclass
 
 @dataclass
 class cpu:
-     brand  :       str
-     prefix :       str
-     gen    :       int
-     mod    :       int
-     suffix :       str 
+    brand   : str
+    prefix  : str
+    gen     : int
+    mod     : int
+    suffix  : str
 
     def __post_init__(self):
         brand_map = {
-        'intl' :    "Intel",
-        'amd'  :     "AMD",
-        'int'  :     "Intel",
+            'intl': "Intel",
+            'amd': "AMD",
+            'int': "Intel",
         }
-    self.brand = brand_map.get(self.brand.lower(),self.brand.capitalize())
-def __str__(self):
-    return f"{self.brand} {self.prefix} {self.gen}{self.mod}{self.suffix}"
+        self.brand = brand_map.get(self.brand.lower(), self.brand.capitalize())
+
+    def __str__(self):
+        return f"{self.brand} {self.prefix} {self.gen}{self.mod}{self.suffix}"
 
 s = cpu('intel','i7',12,700,'k')
 
 print(s)
+
+t = cpu(\
+input('what brand? \nexamples\n\tintl, amd\n\t\tinput:  '),\
+input('which prefix?\nexample\n\tryzen5,i5\n\t\tinput:  '),\
+input("which gen?: "),\
+input("which cpu of the gen?\n\texample: in a i7 12700k, the \"700\" is the model number\n\t\tinput:  "),\
+input("what is the suffex?\n\tin the last exampple, the \"K\" is the suffex\n\t\tinput:  "))
+
+print(t)
